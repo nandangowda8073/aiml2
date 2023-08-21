be61b4194f9ee8430d35031530e54f7f0a7da7b8
nandan <nandangowda8073@gmail.com>
2023-08-21 15:46:37 +0530

    change1

---
+++ d.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+Data=pd.DataFrame([[10,11,12,13],[10,11,12,13]],columns=["maths","science","fc","python"])
+print(Data)
+
+                                                         
